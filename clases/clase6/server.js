271916c93d6a3aaf05b47fe98c7c11b2f771b099
Emilio <emilioplatzer@gmail.com>
2015-10-06 11:18:55 -0300

    Clase 6, empezando el ABM

---
+++ clases/clase6/server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(bodyParser.json());
+
+var productos = [];
+
+app.get("/productos", function (req, res, next) {
+    res.json(productos);
+});
+
+var seq = 0;
+
+app.post("/productos", function (req, res, next) {
+    var _id = "ID" + seq++;
+
+    var producto = req.body;
+    producto._id = _id;
+    productos.push(producto);
+
+    res.status(201).json({
+        _id: _id
+    });
+});
+
+var port=3000;
+
+app.listen(port, function () {
+    console.log("Servidor iniciado en el puerto",port);
+});
+
