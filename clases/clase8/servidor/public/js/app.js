99943b8491a2db090425dd401ac0c551c5a345c3
Gabriel Montes <gabriel@iamat.com>
2015-10-18 18:42:09 -0300

    Integración completa entre app cliente y servidor

--- clases/clase8/servidor/public/js/app.js
+++ clases/clase8/servidor/public/js/app.js
@@ -17,17 +17,53 @@ app.config(function ($routeProvider) {
         });
 });
 
-app.controller("listaLibros", function () {
+app.controller("listaLibros", function ($http, $location) {
     var vm = this;
 
     vm.libros = [];
 
+    $http.get("/api/libros").then(function (response) {
+        vm.libros = response.data;
+    });
+
+    vm.crear = function () {
+        $location.url("/detalle");
+    };
+
+    vm.editar = function (_id) {
+        $location.url("/detalle/" + _id);
+    };
+
+    vm.eliminar = function (_id) {
+        $http.delete("/api/libros/" + _id).then(function (response) {
+            vm.libros = vm.libros.filter(function (libro) {
+                return libro._id !== _id;
+            });
+        });
+    };
 });
 
-app.controller("detalleLibro", function ($routeParams) {
+app.controller("detalleLibro", function ($routeParams, $http, $location) {
     var vm = this;
 
     vm._id = $routeParams.id;
     vm.libro = {};
+    
+    if (vm._id) {
+        $http.get("/api/libros/" + vm._id).then(function (response) {
+            vm.libro = response.data;
+        });
+    }
 
+    vm.modificar = function () {
+        if (vm._id) {
+            $http.put("/api/libros/" + vm._id, vm.libro).then(function (response) {
+                $location.url("/lista");
+            });
+        } else {
+            $http.post("/api/libros", vm.libro).then(function (response) {
+                $location.url("/lista");
+            });
+        }
+    };
 });
