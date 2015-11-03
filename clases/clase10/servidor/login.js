e863d811866afe1de8e326a9a30de0dc429d31ab
Graciela <gracane@gmail.com>
2015-11-03 17:30:42 -0300

    Clase 10 - vemos login de usuarios autenticación y autorización.

---
+++ clases/clase10/servidor/login.js
@@ -0,0 +1,25 @@
+var express = require("express");
+
+var datos = require("./datos");
+
+var router = express.Router();
+
+router.post("/", function (req, res, next) {
+    var nombre = req.body.nombre;
+    var clave = req.body.clave;
+    datos.login.validar(nombre, clave, function (err, userData) {
+        if (err) {
+            next(err);
+            return;
+        }
+        if (userData) {
+            req.session.user = userData;
+            res.end();
+        } else {
+            res.status(401).end();
+        }
+    });
+});
+
+
+module.exports = router;
---
+++ clases/clase10/servidor/server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var session= require("express-session");
+
+var login = require("./login");
+var libros = require("./libros");
+
+var SERVER_PORT = 3000;
+
+var app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(session({
+    cookie: {
+        maxAge:1*60*1000
+    },
+    resave: false,
+    rolling:true,
+    saveUninitialized: true,
+    secret: "COOKIE_SECRET"
+}));
+
+app.use("/api/login", login);
+app.use("/api/libros", libros);
+
+app.listen(SERVER_PORT, function () {
+    console.log("Servidor iniciado en el puerto %s", SERVER_PORT);
+});
