94f83eeeed1315336d88edfb6d3d763c7dd47bf0
Graciela <gracane@gmail.com>
2015-11-03 11:35:35 -0300

    Clase 9 - conexión a la base de datos.

---
+++ clases/clase9/servidor/public/js/app.js
@@ -0,0 +1,64 @@
+var app = angular.module("libros", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/lista", {
+            templateUrl: "vistas/lista.html",
+            controller: "listaLibros",
+            controllerAs: "vm"
+        })
+        .when("/detalle/:id?", {
+            templateUrl: "vistas/detalle.html",
+            controller: "detalleLibro",
+            controllerAs: "vm"
+        })
+        .otherwise({
+            redirectTo: "/lista"
+        });
+});
+
+app.controller("listaLibros", function ($http, $location) {
+    var vm = this;
+    
+    vm.libros = [];
+    $http.get("/api/libros/").then(function(response){
+             vm.libros=response.data;
+    });
+    vm.crear= function() {
+        $location.url("/detalle");
+    };
+    vm.editar= function(_id) {
+        $location.url("/detalle/" +_id);
+    };
+    vm.eliminar= function(_id) {
+       $http.delete("/api/libros/"+ _id).then(function(){
+            $http.get("/api/libros/").then(function(response){
+                vm.libros=response.data;
+            });
+       }); 
+    };
+});
+
+app.controller("detalleLibro", function ($routeParams, $http, $location) {
+    var vm = this;
+
+    vm._id = $routeParams.id;
+    vm.libro = {};
+    if (vm._id){
+        $http.get("/api/libros/"+ vm._id).then(function(response){
+            vm.libro=response.data;
+        });
+    }
+    vm.modificar= function() {
+    if (vm._id){
+        $http.put("/api/libros/"+ vm._id,vm.libro).then(function(){
+            $location.url("/lista");
+        });
+    }else{
+        $http.post("/api/libros/",vm.libro).then(function(){
+             $location.url("/lista");
+        });
+    }    
+    };
+
+});
