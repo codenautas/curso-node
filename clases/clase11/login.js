9fce6aca039e9579ffdd5120fc77132181b29fa1
estefi-capece <eycapece@gmail.com>
2015-11-10 10:24:05 -0300

    Comienzo clase 11

---
+++ clases/clase11/login.js
@@ -0,0 +1,25 @@
+// var crypto=require("crypto");
+// crypto.createHash("sha256").update("1234").digest("hex");
+var express = require("express");
+var datos = require("./datos");
+var router = express.Router();
+
+
+router.post("/",function(req,res,next){
+    var nombre=req.body.nombre;
+    var clave=req.body.clave;
+    datos.login.validar(nombre,clave,function(err,userData){
+        if(err){
+            next(err);
+            return;
+        }
+        if(userData){
+            req.session.user=userData;
+            res.end();
+        }else{
+            res.status(401).end();
+        }
+    });
+});
+
+module.exports=router;
